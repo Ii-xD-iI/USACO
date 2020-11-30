10ecafd9aec0925c9606ce7bd5c918791f0b17f9
The xD <70330230+Ii-xD-iI@users.noreply.github.com>
2020-11-30 14:32:42 +0530

    Create USACO Template.cpp

---
+++ USACO Template.cpp
@@ -0,0 +1,33 @@
+#include "bits/stdc++.h"
+ 
+using namespace std;
+
+#define all(x)   (x).begin(), (x).end()
+#define test     int t;cin>>t;while(t--)
+// #define IO(NAME) ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(0); if(fopen(NAME ".in", "r")) freopen(NAME ".in", "r", stdin),freopen(NAME ".in", "w", stdout); 
+#define len(x)   (int)(x).size()
+#define elif     else if
+#define bignum   1e9+1
+
+//----------------Prototypes------------------
+void IO(string Bessie);
+
+//-----------------Globals--------------------
+
+//-------------------x-x----------------------
+
+int main(){
+	IO("");//Question IO file name Here 
+	
+	
+	
+	return 0;
+}
+
+void IO(string Bessie = "") { 
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    if (len(Bessie)) {
+        freopen((Bessie+".in").c_str(), "r", stdin);
+        freopen((Bessie+".out").c_str(), "w", stdout);
+     }
+}
